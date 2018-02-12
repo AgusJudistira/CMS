ffda0116180b4ed3d24bbc804a6338c98027198f
Agus Judistira <agus.judistira@gmail.com>
2018-02-12 17:19:44 +0100

    [W5-002]

--- CMSbackend_002.php
+++ CMSbackend_002.php
@@ -9,7 +9,57 @@
   </head>
   <body>
     <h1>Blog administratie</h1>
+    <form id="login-form" method="post" action="<?php $thisfile?>">
+            <h3 align='center'>Administrator login</h3>
+            <p>Gebruikersnaam: <input type="text" name="gebruikersnaam" required></p>
+            <p>Wachtwoord: <input type="password" name="wachtwoord" required></p>
+            <p><input type="submit" value="Inloggen"></p>
+    </form>
     <?php
+      session_start();
+      
+      $thisfile = $_SERVER['PHP_SELF'];
+      $uitloggen = "";
+    
+      $editor = "<p>";
+
+      $editor .= "<button onclick='underline()' style='font-size:24px'><i class='material-icons'>format_underlined</i></button>";
+      $editor .= "<button onclick='bolden()' style='font-size:18px'><i class='material-icons'>format_bold</i></button>";
+      $editor .= "<button onclick='italic()' style='font-size:18px'><i class='material-icons'>format_italic</i></button>";
+      $editor .= "<button onclick='insertImage()' style='font-size:18px'><i class='material-icons'>insert_photo</i></button>";
+
+      $editor .= "<button onclick='link()' style='font-size:18px'><i class='material-icons'>insert_link</i></button>";
+      
+      $editor .= "<button onclick='inserthtml()' style='font-size:18px'>HTML invoegen</button>";
+      $editor .= "<button onclick='displayhtml()' style='font-size:18px'>Toon HTML</button>";
+      $editor .= "</p>";
+
+      $editor .= "<form id='artikelinvoer' method='post' action='<?php echo $thisfile; ?>' onsubmit='javascript: return verwerkArtikel();'>";
+      $editor .= "Blogtitel: <input id='blogtitel' name='blogtitel' type='text' value='' title=";
+      $editor .= "\"'Typ '/cg' in om 'Code Gorilla' in te voeren&#013;&#010;";
+      $editor .= "Typ '/ag' in om 'Agus Judistira' in te voeren&#013;&#010;";
+      $editor .= "Typ '/nl' in om 'Nederland' in te voeren&#013;&#010;";
+      $editor .= "Typ '/mvg' in om 'Met vriendelijke groet' in te voeren\" required>";
+      $editor .= "Categorie:";
+      $editor .= "<select name='categorie'>";
+      $editor .= $categoriekeuzemenu;
+      $editor .= "</select>";
+      $editor .= "</form>";
+      $editor .= "<div id='editor' contenteditable='true' spellcheck='false' title=\"Typ '/cg' in om 'Code Gorilla' in te voeren&#013;&#010;";
+      $editor .= "Typ '/ag' in om 'Agus Judistira' in te voeren&#013;&#010;";
+      $editor .= "Typ '/nl' in om 'Nederland' in te voeren&#013;&#010;";
+      $editor .= "Typ '/mvg' in om 'Met vriendelijke groet' in te voeren\">";
+      $editor .= "<p><br />Voer hier een nieuwe blog in...<br /><br /></p>";
+      $editor .= "</div>";
+      $editor .= "<input id='hidden' type='hidden' name='artikel' value='$artikel' form='artikelinvoer'>";
+      $editor .= "<div>";
+      $editor .= "<p><input type='radio' name='commentaar_toegestaan' value='1' checked='checked' form='artikelinvoer'>Commentaar toegestaan</input></p>";
+      $editor .= "<p><input type='radio' name='commentaar_toegestaan' value='0' form='artikelinvoer'>Commentaar uitgeschakeld</input></p>";
+      $editor .= "</div>";
+      $editor .= "<div style='display: inline-flex'>";
+      $editor .= "<input id='sendButton' name='submit' type='submit' value='Blog invoeren' form='artikelinvoer'>";
+      $editor .= "</div>";
+
       /* referentiele integriteit wordt met opzet weggelaten om het eventueel
       verwijderen van records makkelijk te maken */
       require_once "dbconnect.php";
@@ -86,82 +136,108 @@
           $stmt->close();
       }
 
-      $thisfile = $_SERVER['PHP_SELF'];
-      // echo $thisfile;
-      $bloglist = get_blogsandcats();
-      $categoriekeuzemenu = get_categories();
+      function inloggen() {
+          $keuzemenu = "";
+          $db = dbconnect();
 
-      if (isset($_POST['submit'])) {
-        if (isset($_POST['blogtitel'])) {
-          $artikel = $_POST['artikel'];
-          $blogtitel = $_POST['blogtitel'];
-          $cat_id = $_POST['categorie'];
-          $commentaar_toegestaan = $_POST['commentaar_toegestaan'];
+          $gebruikersnaam = $_POST['gebruikersnaam'];
+          $coded_password = md5($_POST['wachtwoord']);
+          $wachtwoord = $_POST['wachtwoord'];
+          /*
+          echo "coded password: $coded_password<br />";
+          echo "gebruikersnaam: $gebruikersnaam<br />";
+          echo "wachtwoord: $wachtwoord<br />";
+          */
+          $stmt = $db->prepare("SELECT id, gebruikersnaam FROM Administrators
+                                WHERE wachtwoord = '$coded_password' AND
+                                      gebruikersnaam = '$gebruikersnaam';");
 
-          $lastid = insert_blog($blogtitel, $artikel, $commentaar_toegestaan);
-          insert_category($lastid, $cat_id);
+          $stmt->execute();
+          $stmt->bind_result($id, $gebruikersnaam);
+          $stmt->store_result();
 
-          $bloglist = get_blogsandcats();
-        }
+          //echo "number of rows: $stmt->num_rows";
+          if ($stmt->num_rows > 0) {
+              $_SESSION['gebruikersnaam'] = $gebruikersnaam;
+              ?>
+              <script type="text/javascript">
+                  document.getElementById('login-form').style.display = "none";                  
+              </script>
+              <?php
+          }          
       }
 
+      function uitloggen() {
+          session_unset();
+          session_destroy();
+      }
+
+      if (isset($_POST['uitloggen'])) {
+          uitloggen();
+          $uitloggen = "";
+      }
+
+      if (isset($_POST['gebruikersnaam'])) {
+          inloggen();
+      }
+
+      if (!isset($_SESSION['gebruikersnaam'])) {
+          $editor = "";
+        ?>
+          <script type="text/javascript">
+              
+              document.getElementById('login-form').style.display = "block";
+              //alert('nog niet ingelogd');
+          </script>
+        <?php          
+      }
+      else {
+        ?>
+          <script type="text/javascript">
+              document.getElementById('login-form').style.display = "none";
+          </script>
+        <?php
+        if (isset($_POST['submit'])) {
+          if (isset($_POST['blogtitel'])) {
+            $artikel = $_POST['artikel'];
+            $blogtitel = $_POST['blogtitel'];
+            $cat_id = $_POST['categorie'];
+            $commentaar_toegestaan = $_POST['commentaar_toegestaan'];
+
+            $lastid = insert_blog($blogtitel, $artikel, $commentaar_toegestaan);
+            insert_category($lastid, $cat_id);
+
+            //$bloglist = get_blogsandcats();
+
+          }
+        }
+        $uitloggen .= "<form id='uitloggen-form' method='post' action='$thisfile'>";
+        $uitloggen .= "<p><input type='submit' name='uitloggen' value='Uitloggen'></p>";
+        $uitloggen .= "</form>";
+        $bloglist = get_blogsandcats();
+        $categoriekeuzemenu = get_categories();
+      }
 
     ?>
-    <div class="row">
+
+    <div class="container">
       <div id="linkerkolom">
+        <?php //echo "wachtwoord:" . md5('medLey'); ?>
         <h3><a href="CMSbackendcategory_002.php">Categorie toevoegen</a></h3>
         <h3><a href="CMSfrontend_002.php">Naar de voorkant</a></h3>
+        <?php echo $uitloggen; ?>
       </div>
       <div id="rechterkolom">
         <?php echo $bloglist; ?>
-        <p>
-
-          <button onclick="underline()" style="font-size:24px"><i class="material-icons">format_underlined</i></button>
-          <button onclick="bolden()" style="font-size:18px"><i class="material-icons">format_bold</i></button>
-          <button onclick="italic()" style="font-size:18px"><i class="material-icons">format_italic</i></button>
-          <button onclick="insertImage()" style="font-size:18px"><i class="material-icons">insert_photo</i></button>
-
-          <button onclick="link()" style="font-size:18px"><i class="material-icons">insert_link</i></button>
-          <!-- <button onclick="link()">Link</button> -->
-          <button onclick="inserthtml()" style="font-size:18px">HTML invoegen</button>
-          <button onclick="displayhtml()" style="font-size:18px">Toon HTML</button>
-        </p>
-
-        <form id="artikelinvoer" method="post" action="<?php echo $thisfile; ?>" onsubmit="javascript: return verwerkArtikel();">
-        Blogtitel: <input id="blogtitel" name="blogtitel" type="text" value="" title=
-        "Typ '/cg' in om 'Code Gorilla' in te voeren&#013;&#010;
-  Typ '/ag' in om 'Agus Judistira' in te voeren&#013;&#010;
-  Typ '/nl' in om 'Nederland' in te voeren&#013;&#010;
-  Typ '/mvg' in om 'Met vriendelijke groet' in te voeren" required>
-        Categorie:
-          <select name="categorie">
-            <?php
-            echo $categoriekeuzemenu;
-            ?>
-          </select>
-        </form>
-
-        <div id="editor" contenteditable="true" spellcheck="false" title="Typ '/cg' in om 'Code Gorilla' in te voeren&#013;&#010;
-Typ '/ag' in om 'Agus Judistira' in te voeren&#013;&#010;
-Typ '/nl' in om 'Nederland' in te voeren&#013;&#010;
-Typ '/mvg' in om 'Met vriendelijke groet' in te voeren">
-            <p><br />Voer hier een nieuwe blog in...<br /><br /></p>
-        </div>
-        <input id="hidden" type="hidden" name="artikel" value="<?php $artikel ?>" form="artikelinvoer">
-
-          <div>
-              <p><input type="radio" name="commentaar_toegestaan" value="1" checked="checked" form="artikelinvoer">Commentaar toegestaan</input></p>
-              <p><input type="radio" name="commentaar_toegestaan" value="0" form="artikelinvoer">Commentaar uitgeschakeld</input></p>
-          </div>
-          <div style="display: inline-flex">
-              <input id="sendButton" name="submit" type="submit" value="Verstuur" form="artikelinvoer">
-          </div>
+        <?php echo $editor; ?>
+      </div>
+    
+      <div id="hidden-h2" style="display: none"> 
+        <h2>nodig</h2>
+      </div>
+      <div id="comment-checkbox">
+      </div>
     </div>
-  </div>
-    <div id="comment-checkbox">
-
-    </div>
-
     <script src="CMSbackend_002.js"></script>
     <script src="wysiwyg-editor.js"></script>
   </body>
